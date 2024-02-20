8186742016e76c18e1c9e01ce8c29bc0934fba26
Rabota Denys <dn181095rda@gmail.com>
2024-02-20 09:31:03 +0200

    20/02/2024

--- bugsBot.js
+++ bugsBot.js
@@ -8,147 +8,159 @@ const bot = new TelegramBot(BOT_TOKEN, {
   polling: true,
 });
 
-// Объект для отслеживания состояния пользователя
-const userState = {};
+const updateTask = ( task_id, data, chat_id, text) => {
+  try{
+    axios.post(`${url + task_id}`, data, {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${PF_TOKEN}`,
+      },
+    })
+    .then(() => {
+      if(!!chat_id){
+        bot.sendMessage(chat_id,text);
+      }
+    })
+  }
+  catch{
+    console.log('Ошибка:', error);
+  }
+}
 
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const message = msg.text;
-
-  if (userState[chatId] === "processing" || userState[chatId] === "reasoning") {
-
-  } else {
-    // Обрабатываем первый ответ пользователя
-    // Отправка данных на сервер
-    const send_data = {
-      customFieldData: [
-        {
-          field: {
-            id: 40134,
-          },
-          value: message, // Отправляем сообщение пользователя в поле id: 40134
-        },
-      ],
-    };
-
-    try {
-      await axios.post(`${url + splited_data[1]}`, send_data, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${PF_TOKEN}`,
-        },
-      });
-
-      // Ваши дополнительные действия после успешной отправки данных на сервер
-      // Здесь можно отправить сообщение "Запрос принят в обработку" или что-то еще
-
-      // Устанавливаем состояние "processing" только после успешной отправки
-      userState[chatId] = "processing";
-    } catch (error) {
-      console.error("Ошибка:", error);
+const dataTypeStatus = (id) => {
+  return {
+    "status": {
+      "id": id
     }
+  }
+}
+
+const dataTypeLine = (field_id, value) => {  // вернуть данные для поля типа Line
+  return {
+    customFieldData: [
+      {
+        field: {id: field_id},
+        value: value,
+      },
+    ],
+  }
+}
+
+function waitForAnswer(chatId) {
+  return new Promise((resolve, reject) => {
+    bot.onText(/[^a-zA-Z]/g, (msg, match) => {
+      const messageChatId = msg.chat.id;
+      if (messageChatId === chatId) {
+        const answer = match.input
+        resolve(answer);
+      }
+    });
+  });
+}
+
+const checkNumber = async (number, updateFunc, task_id, send_data, field_id, chat_id, text, field_id2) => {
+  const num = Number(number);
+  if (!num) {
+    bot.sendMessage(chat_id, "Пожалуйста указжите сумму только цифрами без букв и символов!")
+    .then(async () => {
+      await waitForAnswer(chat_id)
+      .then(async (answer) => {
+       await checkNumber(answer, updateFunc, splited_data[1], send_data, field_id, chat_id, text, field_id2)
+      })
+    })
+  } else {
+    await updateTask(task_id, send_data(field_id, num))
+    await bot.sendMessage(chat_id, text)
+    .then(() => {
+      waitForAnswer(chat_id)
+      .then((answer) => {
+        updateTask(task_id, send_data(field_id2, answer), chat_id, "Спасибо! После поступления суммы на счет вы получите фидбек от Фин. Мендежера")
+      })
+    })
+  }
+}
+
+bot.on('message', (msg) => {
+  const chatId = msg.chat.id;
+  const message = msg.text;   
+  
+  if (message.indexOf("DrewSkow")>-1){
+    bot.sendMessage(chatId,"Привет, откуда ты знаешь эту булочку?")
+    .then(() => {
+      waitForAnswer(chatId)
+      .then(()=>{
+        bot.sendMessage(chatId,"Ты, кстати, тоже 100% сладкая булочка)")
+      })
+    })
   }
 });
 
-bot.on("callback_query", (callbackQuery) => {
+bot.on('callback_query', (callbackQuery) => {
   const chatId = callbackQuery.message.chat.id;
   const data = callbackQuery.data;
   const messageId = callbackQuery.message.message_id;
-  userState[chatId] = null;
 
-  bot
-    .editMessageReplyMarkup(
-      {
-        inline_keyboard: [], // Удаляет клавиатуру
-      },
-      {
-        chat_id: chatId,
-        message_id: messageId,
-      }
-    )
-    .catch((error) => {
-      console.error("Ошибка:", error);
-    });
+  bot.editMessageReplyMarkup({ inline_keyboard: [] }, { chat_id: chatId, message_id: messageId })
+  .catch((error) => {
+    console.error('Ошибка:', error);
+  });
 
   splited_data = data.split(":");
   if (splited_data[0] == "set_true") {
-    // Обработка согласия
-    const send_data = {
-      customFieldData: [
-        {
-          field: {
-            id: 40134,
-          },
-          value: "Согласовано, можем предоставить экспорт",
-        },
-      ],
-    };
-
-    axios
-      .post(`${url + splited_data[1]}`, send_data, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${PF_TOKEN}`,
-        },
-      })
-      .then(() => {
-        // Ваши дополнительные действия после успешного согласия
-        bot.sendMessage(chatId, "Запрос принят в обработку");
-        userState[chatId] = "processing"; // Устанавливаем состояние "processing" после согласия
-      })
-      .catch((error) => {
-        console.error("Ошибка:", error);
-      });
-  } else if (splited_data[0] == "set_false") {
-    if (userState[chatId] === "processing") {
-      bot.sendMessage(chatId, "Запрос уже был принят, активных задач нет");
-    } else {
-      // Обработка отказа
-      bot.sendMessage(
-        chatId,
-        "Укажите, пожалуйста, причину отказа или уточнения для постановщика"
-      );
-
-      // Устанавливаем состояние "reasoning" для ожидания ответа пользователя
-      userState[chatId] = "reasoning";
-    }
+    updateTask(splited_data[1], dataTypeLine(40134, "Согласовано, можем предоставить экспорт"), chatId, "Запрос принят в обработку")
   }
-});
-
-bot.on("message", async (msg) => {
-  const chatId = msg.chat.id;
-  const message = msg.text;
-
-  // Проверяем, находится ли пользователь в состоянии "reasoning"
-  if (userState[chatId] === "reasoning") {
-    // Пользователь ответил на запрос причины отказа
-    const answer = message;
-
-    // Отправляем ответ на отказ и записываем в поле id: 40134
-    const send_data = {
-      customFieldData: [
-        {
-          field: {
-            id: 40134,
-          },
-          value: answer,
-        },
-      ],
-    };
-
-    try {
-      await axios.post(`${url + splited_data[1]}`, send_data, {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${PF_TOKEN}`,
-        },
-      });
-
-      // Ваши дополнительные действия после получения ответа на отказ
-      bot.sendMessage(chatId, "Запрос принят в обработку");
-      userState[chatId] = "processing"; // Устанавливаем состояние "processing" после отказа
-    } catch (error) {
-      console.error("Ошибка:", error);
-    }
+  
+  if (splited_data[0] == "set_false") {
+    
+    bot.sendMessage(chatId,"Укажите, пожалуйста, причину отказа или уточнения для постановщика")
+     .then(() => {
+      waitForAnswer(chatId)
+      .then((answer) => {
+        updateTask(splited_data[1], dataTypeLine(40134, answer), chatId, "Спасибо, мы сообщим постановщику");
+      })
+    })
   }
-});
+
+  if(splited_data[0] == "accept_statistics"){
+    updateTask(splited_data[1], dataTypeLine(40918,"[Со статистикой ознакомился!]" ), chatId, "Спасибо за ответ! Хорошего дня");
+  }
+
+  if(splited_data[0] == "refill_successfully"){
+    updateTask(splited_data[1], dataTypeStatus(2));
+
+    bot.sendMessage(chatId,"Пожалуйста приложите ссылку на чек")
+    .then(() => {
+      waitForAnswer(chatId)
+      .then((answer) => {
+        updateTask(splited_data[1], dataTypeLine(36772, answer), chatId, "Спасибо! После поступления суммы на счет вы получите фидбек от Фин. Мендежера");
+      }) 
+    })
+
+  }
+
+  if(splited_data[0] == "refill_unsuccessfully") {
+    bot.sendMessage(chatId,"Укажите причину отказа")
+    .then(() => {
+      waitForAnswer(chatId)
+      .then((answer) => {
+        updateTask(splited_data[1], dataTypeLine(40918, `${"[Пополение отклонено] " + answer}`), chatId, "Принял твой ответ, ожидай фидбека от Фин. Менеджера");
+      })
+    })
+  }
+
+  if(splited_data[0] == "update_successfully") {
+    bot.sendMessage(chatId,"Сумма утверждена");
+    updateTask(splited_data[1], dataTypeLine(40918, "[Актуальная сумма утверждена]"));
+  }
+
+  if(splited_data[0] == "update_unsuccessfully") {
+    bot.sendMessage(chatId,"Опишите причину несогласия:")
+    .then(() => {
+      waitForAnswer(chatId)
+      .then((answer) => {
+        updateTask(splited_data[1], dataTypeLine(40918, `${"[Актуальная сумма не утверждена] " + answer}`), chatId, "Спасибо, ожадайти фидбека от Фин. Мендежера");
+      })
+    })
+  }
+})
+
